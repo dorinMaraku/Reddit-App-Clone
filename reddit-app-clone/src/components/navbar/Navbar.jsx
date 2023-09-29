0ef6fff45c67e615b13f91e856ec9204980f24e9
dorinMaraku <dorinmaraku@gmail.com>
2023-09-29 14:42:34 +0200

    starting reddit clone app

---
+++ reddit-app-clone/src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import RedditIcon from '@mui/icons-material/Reddit';
+import SearchIcon from '@mui/icons-material/Search';
+import './Navbar.css'
+
+const Navbar = () => {
+
+  return (
+    <nav className="nav-container">
+        <div className='nav-logo'> 
+            <RedditIcon className='reddit-icon'/>
+            <h1 className='nav-logo-text' >reddit<span className='nav-clone-text'>Clone</span></h1>
+        </div>
+        <div className='nav-searchbar'>
+            <form className='input-form'>
+                <button className='search-btn'><SearchIcon/></button>
+                <input className='search-input' type='text' placeholder='Search Reddit' />
+            </form>
+        </div>
+    </nav>
+  )
+}
+export default Navbar
