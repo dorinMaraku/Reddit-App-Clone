fe1500720146aa2ec456eebf0c4263e1ae1223e2
dorinMaraku <dorinmaraku@gmail.com>
2023-10-03 17:38:21 +0200

    added auhentication and got user info from api

--- reddit-app-clone/src/App.jsx
+++ reddit-app-clone/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import FeedList from './components/feedlist/FeedList'
 import Login from './components/login/Login'
+import Home from './components/home/Home'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />}/>
       </Routes>
       <FeedList />
     </>
